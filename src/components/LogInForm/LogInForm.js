5789007931675cf542f8aab97478678fbde59dbf
SpInAgRblzzz <spinagrblzzz@gmail.com>
2020-07-07 15:49:21 +0300

    feat: redirect added

--- src/components/LogInForm/LogInForm.js
+++ src/components/LogInForm/LogInForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Redirect } from "react-router-dom";
 
 function LogInForm() {
 	const [inputValue, setInputValue] = useState("");
@@ -19,6 +20,7 @@ function LogInForm() {
 				value={inputValue}
 			/>
 			<input type="submit" value="LogIn"></input>
+			<Redirect to="/login" />
 		</form>
 	);
 }
