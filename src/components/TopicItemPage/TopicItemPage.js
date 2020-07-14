01a20e116510650ddc77b1c79721562d141fc283
SpInAgRblzzz <spinagrblzzz@gmail.com>
2020-07-14 14:13:33 +0300

    feat: topic page requests added

--- src/components/TopicItemPage/TopicItemPage.js
+++ src/components/TopicItemPage/TopicItemPage.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+const axios = require("axios");
 
 function TopicItemPage() {
-	return <p>topic page</p>;
+	const [pageContent, setPageContent] = useState({});
+	const { topic, id } = useParams();
+
+	useEffect(() => {
+		axios
+			.get(`https://anapioficeandfire.com/api/${topic}/${id}`)
+			.then(({ data }) => {
+				console.log(data);
+				setPageContent(data);
+			});
+	}, [topic, id]);
+
+	return <p>{pageContent.name ? pageContent.name : `noname`}</p>;
 }
 
 export default TopicItemPage;
